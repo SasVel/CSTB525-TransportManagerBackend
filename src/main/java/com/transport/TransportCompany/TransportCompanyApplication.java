2a29425d09a02f4d3f36e52eac24eebd62105bbe
SasVel <sasvelchev@gmail.com>
2026-01-08 12:30:05 +0200

    Switched template, connected to DB

---
+++ src/main/java/com/transport/TransportCompany/TransportCompanyApplication.java
@@ -0,0 +1,11 @@
+package com.transport.TransportCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportCompanyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TransportCompanyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/transport/TransportCompany/TransportCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.TransportCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
